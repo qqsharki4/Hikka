16afb572374a61bf475a1a62ab1d90606c6dc6f5
qqsharki4 <159995328+qqsharki4@users.noreply.github.com>
2025-06-24 21:18:31 +0700

    Update version.py

--- hikka/version.py
+++ hikka/version.py
@@ -7,13 +7,5 @@
 
 __version__ = (1, 6, 3)
 
-import os
-
-import git
-
-try:
-    branch = git.Repo(
-        path=os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    ).active_branch.name
-except Exception:
-    branch = "master"
+# Убрана зависимость от Git, ветка фиксирована как 'main'
+branch = "master"
